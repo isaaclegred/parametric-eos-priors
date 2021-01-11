9649f05a5ac202e7278e748f1280b5649583f47c
Isaac Legred <inl7@cornell.edu>
2021-01-10 21:07:38 -0800

    updates to drawing

--- make_eos_draws.sh
+++ make_eos_draws.sh
@@ -1,16 +1,18 @@
 eos_per_dir=$1
-# This is one higher than expected because the top of the for loop is inclusive
 num_mod_dirs=$2
 eos_dir_name=$3
 
 mkdir $eos_dir_name
 cd $eos_dir_name
-for index in $(seq -f '%06g' 0 $num_mod_dirs)
+for index in $(seq -f '%06g' 1 $num_mod_dirs)
 do
     dir=DRAWmod$eos_per_dir-$index
     mkdir $dir
     cd $dir
-    python3 ../../draw_eos_uniform.py --num-draws $eos_per_dir --dir-index $index
+    # This is a bit of a problem, I don't really want to 
+    # write code for python 2, but much of the code
+    # I'm using is incompatible with python 3
+    python3 ../../draw_eos_uniform.py --num-draws $eos_per_dir 
     cd .. 
 done
 cd ..
