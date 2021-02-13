b306b0e8e7d2406b1aa185b09e3b4338387dfca1
Isaac Legred <isaac.legred@ldas-pcdev2.ligo.caltech.edu>
2021-02-12 19:21:43 -0800

    Add plotting functionality and helpful scripts

--- Utils/make_prior_csv.py
+++ Utils/make_prior_csv.py
@@ -10,15 +10,25 @@ parser.add_argument('--posterior-file-path', dest="source_path", default="./tota
 
 parser.add_argument('--output', dest="output", default="./total_prior.csv",
                     help = "Where to create the output (prior) csv file ")
+parser.add_argument('--num-weight-columns', dest="num_weights", default=-1, type=int,
+                    help="Number of logweight columns to copy")
 args = parser.parse_args()
 
 source_path = args.source_path
 output_path = args.output
+num_weights = args.num_weights
 f = open(source_path)
 source_header = f.readline()
 f.close() 
 source_data = np.loadtxt(source_path,skiprows=1,delimiter=",")
 eos_indices = source_data[:,0]
-prior_data = np.ones_like(source_data)
-prior_data[:,0] = eos_indices
+if (num_weights == -1):
+    prior_data = np.ones_like(source_data)
+    prior_data[:,0] = eos_indices
+else:
+    weight_reps  = np.ones_like(source_data[:,1:num_weights+1])
+    prior_data = source_data
+    prior_data[:,0] = eos_indices
+    prior_data[:,1:num_weights+1] = weight_reps
+    
 np.savetxt(output_path, prior_data,  header = source_header, comments="", delimiter=",")
