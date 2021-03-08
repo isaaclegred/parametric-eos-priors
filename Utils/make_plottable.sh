0019b4386d6a351f4bbbb58fdbe2b365a03d2a28
Isaac Legred <isaac.legred@ldas-grid.ligo-wa.caltech.edu>
2021-03-07 21:29:16 -0800

    correct a file and add a script to plot a single covariance plot (apply everywhere)

--- Utils/make_plottable.sh
+++ Utils/make_plottable.sh
@@ -1,15 +1,19 @@
 NUM_WEIGHTS=6
-TAGS="parametric" "nonparametric"
+TAGS="parametric nonparametric"
 for TAG in $TAGS
 do
+    # This is kinda silly, I think something needs to be refactored to make
+    # everything make sense here, for example scripts should be different
+    # from functions.
     python3 ~/parametric-eos-priors/Utils/correct_column_names.py \
         --posterior-file-path {$TAG}_post.csv \
         --output corrected_{$TAG}_post.csv \
         --num-weight-columns $NUM_WEIGHTS
 done
-
+# I don't know about this either
+cd Utils/Plotting
 condor_submit add_submit
-
+cd ../..
 
 
 
