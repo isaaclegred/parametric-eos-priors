21f74d3699090c1dd55d08cec67ea05ce0bacef2
Isaac Legred <ilegred@caltech.edu>
2021-08-09 16:17:18 -0700

    put in code that was ignored by gitignore

---
+++ bin/make_eos_draws.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+dir_index=$1
+dirs_to_make=$2
+eos_per_dir=$3
+eos_dir_name=$4
+prior_info=$5
+
+
+
+((min_index=dir_index*dirs_to_make))
+((max_index=min_index + dirs_to_make - 1))
+
+eos_model=$(echo $prior_info| cut  -d " " -f1)
+prior_tag=$(echo $prior_info| cut  -d " " -f2)
+# Will probably want to add more executables
+model_executable=""
+if [[ $eos_model == "*iecewise*" ]]; then
+    model_executable=$HOME"/parametric-eos-priors/draw_eos_piecewise.py"
+if [[ $eos_model == "*os*"]]
+else 
+    model_executable=$HOME"/parametric-eos-priors/draw_eos_spectral.py"
+fi
+mkdir $eos_dir_name
+cd $eos_dir_name
+
+for raw_index in $(seq $min_index $max_index)
+do
+    # Should just be able to do seq -f %06g $min_index $max_index
+    # but for some reason this doesn't work here
+    index=$(printf "%06d" $raw_index)
+    dir=DRAWmod$eos_per_dir-$index
+    mkdir $dir
+    cd $dir
+    # This is a bit of a problem, I don't really want to 
+    # write code for python 2, but much of the code
+    # I'm using is incompatible with python 3
+    python3.7 $model_executable --num-draws $eos_per_dir --dir-index $index --prior-tag $prior_tag
+    cd .. 
+done
+cd ..
